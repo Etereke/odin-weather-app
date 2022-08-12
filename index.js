acd96ede497c3587ce910a911aa2a4a56a44ad06
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-08-12 17:55:08 +0200

    Finish project

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-async function GetWeatherData(location) {
-  const weatherDataRaw = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location.toLowerCase()}&APPID=3300a32f8dd9d1fc070bc54499a5b81d&units=metric`,
-    { mode: "cors" }
-  );
-  const weatherDataJSON = await weatherDataRaw.json();
-  return weatherDataJSON;
-}
 class WeatherObject {
   constructor(city, temp, hum, wind, weather) {
     this.city = city;
@@ -22,6 +14,16 @@ class WeatherObject {
     }
   }
 }
+
+async function GetWeatherData(location) {
+  const weatherDataRaw = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location.toLowerCase()}&APPID=3300a32f8dd9d1fc070bc54499a5b81d&units=metric`,
+    { mode: "cors" }
+  );
+  const weatherDataJSON = await weatherDataRaw.json();
+  return weatherDataJSON;
+}
+
 async function GetWeatherObject(location) {
   const weatherDataJSON = await GetWeatherData(location);
   if (weatherDataJSON.cod === "404") {
@@ -39,9 +41,25 @@ async function GetWeatherObject(location) {
   }
 }
 
-async function logWeatherData(location) {
-  const weather = await GetWeatherObject(location);
-  console.log(weather);
+async function setImgSrc(weatherString) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=3Fj8LpWa2cnlbDLNxNph937V1axOJ2XY&s=weather: ${weatherString}`,
+    { mode: "cors" }
+  );
+  const gif = await response.json();
+  img.src = gif.data.images.original.url;
+}
+
+function DisplayWeather() {
+  city.textContent = weatherObj.city;
+  temperature.textContent = fahren.checked
+    ? weatherObj.GetTemperature(false) + " °F"
+    : weatherObj.GetTemperature(true) + " °C";
+  humidity.textContent = weatherObj.humidity;
+  wind.textContent = weatherObj.wind + " m/s";
+  weather.textContent = weatherObj.weather;
+  img.hidden = false;
+  setImgSrc(weatherObj.weather);
 }
 
 const input = document.querySelector("#city");
@@ -51,8 +69,10 @@ const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 const weather = document.querySelector(".weather");
-
+const img = document.querySelector(".illustration");
+const checkBox = document.querySelector("#fahren");
 let weatherObj;
+
 weatherBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   if (input.validity.valueMissing) {
@@ -61,11 +81,7 @@ weatherBtn.addEventListener("click", async (e) => {
     const weatherTempObj = await GetWeatherObject(input.value);
     if (weatherTempObj) {
       weatherObj = weatherTempObj;
-      city.textContent = weatherObj.city;
-      temperature.textContent = weatherObj.GetTemperature(true);
-      humidity.textContent = weatherObj.humidity;
-      wind.textContent = weatherObj.wind;
-      weather.textContent = weatherObj.weather;
+      DisplayWeather();
     }
   }
 });
